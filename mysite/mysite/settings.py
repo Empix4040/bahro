2d6cefd53cebb9234a499cae7df831a6a0371fe2
Empix4040 <83436473+Empix4040@users.noreply.github.com>
2021-11-05 17:19:13 +0100

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -118,14 +118,18 @@ USE_TZ = True
 import os 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
 MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),
-]
+
 
 
 # Email settings for

152496fbb747b0c639f0c6e41265919b5dfd4a59
Empix4040 <83436473+Empix4040@users.noreply.github.com>
2021-12-01 22:35:53 +0100

    1.12.2021

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,3 +1,4 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render 
 from django.core.mail import send_mail
 from .models import Category,Photo
@@ -5,18 +6,35 @@ from django.http import HttpResponse
 
 def portfolio(request):
     category = request.GET.get('category')
-    
+    description =""
     if category == None:
         photos = Photo.objects.all()
-    else:
+        description = ""
+    else:    
         photos =Photo.objects.filter(category__name__contains=category)
+        ime= Category.objects.get(name = category)
+        name = ime.name
+        print(name)
+        if category == name:
+            categories = Category.objects.get(name = category)
+            description = (
+                 categories.description,
+            )
+            description = ','.join(str(o) for o in description)
+
+        else:
+            print("ERROR NO DESCRIPTION FOUND")
+            description = ""
         
+
     categories = Category.objects.all()
-     
+   
     
     return render(request,"portfolio.html",{
        "categories": categories,
        "photos": photos,
+       "description": description,
+       
  
        })
 def contact(request):
